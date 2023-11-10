8f4e3c6ddd6cb22729d126cd7941ffd948f41d0c
ValentynaSavchenko <149622417+ValentynaSavchenko@users.noreply.github.com>
2023-11-10 11:36:44 +0200

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,16 @@
+"""
+Задача№3
+
+Напишіть програму для друку літери Р висотою 5 рядків за допомогою
+введеного користувачем символу.
+
+Автор: Савченко Валентина
+"""
+
+symbol = input("Уведіть символ для друку літери: ")
+
+print( f'{symbol} {symbol} {symbol} {symbol}')
+print( f'{symbol}     {symbol}')
+print( f'{symbol} {symbol} {symbol} {symbol}')
+print( symbol)
+print( symbol)
